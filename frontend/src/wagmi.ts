803992c945aa3a0808c2985fe1cbe992f62bf1ff
LeTamanoir <martin.saldinger@gmail.com>
2024-02-24 15:19:10 +0100

    init repo

---
+++ frontend/src/wagmi.ts
@@ -0,0 +1,17 @@
+import { http, createConfig } from "wagmi";
+import { holesky } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
+
+export const config = createConfig({
+  chains: [holesky],
+  connectors: [injected()],
+  transports: {
+    [holesky.id]: http(),
+  },
+});
+
+declare module "wagmi" {
+  interface Register {
+    config: typeof config;
+  }
+}
