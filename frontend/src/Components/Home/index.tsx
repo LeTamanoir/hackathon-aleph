b4f8be3af0aaeb3a32b86e41ab25c000bbdcd0d2
agherasie <alexandru.gherasie@epitech.eu>
2024-02-24 20:45:53 +0100

    feat: selecting safe

--- frontend/src/Components/Home/index.tsx
+++ frontend/src/Components/Home/index.tsx
@@ -1,16 +1,65 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [safe, setSafe] = useState<`0x${string}` | null>(null);
+  console.log({ safe });
+
   return (
     <div className="width-100%">
-      <div className="p-20 text-center">
-        <div className="text-8xl flex-row flex m-auto w-fit space-x-2">
-          <h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-pink-600 m-auto">
-            SAFU
-          </h1>
-          <h1 className="font-extrabold text-7xl text-white m-auto">Wallet</h1>
+      <div className="p-20 text-center space-y-10">
+        <div className="space-y-2">
+          <div className="text-8xl flex-row flex m-auto w-fit space-x-4">
+            <h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-pink-600 m-auto">
+              SAFU
+            </h1>
+            <h1 className="font-extrabold text-6xl text-white m-auto">
+              {"{wallet}"}
+            </h1>
+          </div>
+          <p className="text-base text-light italic text-gray-400">
+            Powered by Aleph.im
+          </p>
         </div>
-        <p className="text-base text-light italic" style={{ color: "#B9B9B9" }}>
-          Powered by Aleph.im
-        </p>
+        <div className="text-gray-300 space-y-4">
+          {safe && (
+            <button className="w-fit m-auto hover:scale-110 duration-500">
+              <p className="text-xl" onClick={() => setSafe(null)}>
+                Change Safe
+              </p>
+            </button>
+          )}
+          {!safe && (
+            <p className="text-xl inline-block align-top px-1">
+              Please select a safe :
+            </p>
+          )}
+          <div className="flex justify-center flex-col space-y-2 w-1/3 m-auto duration-500">
+            {[
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093e",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093f",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093g",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093h",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093i",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093j",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093k",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093l",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093m",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093n",
+              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093o",
+            ]
+              .filter((address) => !safe || address === safe)
+              .map((address) => (
+                <div className="bg-gradient-to-r from-blue-400 to-pink-600 p-3 text-slate-300  border-black outline-1 rounded-xl bg-slate-500 hover:cursor-pointer hover:bg-slate-600 hover:scale-105 duration-500">
+                  <p
+                    onClick={() => setSafe(address as `0x${string}`)}
+                    className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+                  >
+                    {address}
+                  </p>
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );
