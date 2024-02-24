1d3bd335bbab6ffc1cbf809fe3c2ed21d54f41d7
lennyvongphouthone <lenny.vongphouthone@epitech.eu>
2024-02-25 00:11:25 +0100

    wip

---
+++ EthereumAlephInnovativeIntegration/src/EthereumAlehSync.sol
@@ -0,0 +1,19 @@
+/**
+ *Submitted for verification at Etherscan.io on 2020-12-18
+*/
+
+pragma solidity ^0.5.11;
+
+contract EthereumAlephSync{
+
+    event SyncEvent(uint256 timestamp, address addr, string message);
+    event MessageEvent(uint256 timestamp, address addr, string msgtype, string msgcontent);
+
+    function doEmit(string memory message) public {
+        emit SyncEvent(block.timestamp, msg.sender, message);
+    }
+
+    function doMessage(string memory msgtype, string memory msgcontent) public {
+        emit MessageEvent(block.timestamp, msg.sender, msgtype, msgcontent);
+    }
+}
