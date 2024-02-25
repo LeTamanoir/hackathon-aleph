3ad17e4e77a928a304a5a4744991d3d4e55ecfd6
LeTamanoir <martin.saldinger@gmail.com>
2024-02-25 01:36:55 +0100

    add background

--- frontend/src/Components/Home/index.tsx
+++ frontend/src/Components/Home/index.tsx
@@ -1,66 +1,40 @@
-import { useState } from "react";
+import AlephIcon from "../Header/AlephIcon";
 
 export default function Home() {
-  const [safe, setSafe] = useState<`0x${string}` | null>(null);
-  console.log({ safe });
-
   return (
-    <div className="width-100%">
-      <div className="p-20 text-center space-y-10">
-        <div className="space-y-2">
-          <div className="text-8xl flex-row flex m-auto w-fit space-x-4">
-            <h1 className="font-extrabold text-transparent select-none bg-clip-text bg-gradient-to-r from-blue-700 to-blue-500 m-auto">
-              SAFU
-            </h1>
-            <h1 className="h-16 text-transparent select-none bg-clip-text bg-gradient-to-r from-gray-100 to-gray-400 text-6xl text-white m-auto">
-              {"{wallet}"}
-            </h1>
-          </div>
-          <p className="text-base text-light select-none italic text-gray-400">
-            Powered by Aleph.im
-          </p>
+    <div
+      className="width-full flex flex-col gap-10 items-center justify-center"
+      id="home-page"
+    >
+      <div className="flex flex-col gap-2">
+        <div className="text-8xl flex-row flex m-auto w-fit gap-x-4">
+          <h1 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-blue-500 m-auto">
+            SAFU
+          </h1>
+          <h1 className="h-16 text-transparent bg-clip-text bg-gradient-to-r from-gray-100 to-gray-400 text-6xl text-white m-auto">
+            {"{wallet}"}
+          </h1>
         </div>
-        <div className="text-gray-300 space-y-4">
-          {safe && (
-            <button className="w-fit m-auto hover:scale-110 duration-200">
-              <p className="text-xl" onClick={() => setSafe(null)}>
-                Change Safe
-              </p>
-            </button>
-          )}
-          {!safe && (
-            <p className="text-xl inline-block align-top px-1">
-              Please select a safe :
-            </p>
-          )}
-          <div className="flex justify-center flex-col space-y-2 w-1/3 m-auto duration-200">
-            {[
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093e",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093f",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093g",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093h",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093i",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093j",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093k",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093l",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093m",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093n",
-              "0x4EF0ef5aC60ae6117AF59A5ea1F90Ad2e6ce093o",
-            ]
-              .filter((address) => !safe || address === safe)
-              .map((address) => (
-                <div className="bg-gradient-to-r from-blue-700 to-blue-500 p-3 text-slate-300  border-black outline-1 rounded-xl bg-slate-500 hover:cursor-pointer hover:bg-slate-600 hover:scale-105 duration-500">
-                  <p
-                    onClick={() => setSafe(address as `0x${string}`)}
-                    className="drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
-                  >
-                    {address}
-                  </p>
-                </div>
-              ))}
-          </div>
+        <div className="text-base justify-center flex items-center text-light italic text-gray-400">
+          <span>Powered by</span>
+          <AlephIcon className="size-4 inline ml-1.5 mr-0.5" />
+          <span>Aleph.im</span>
         </div>
       </div>
+
+      <p className="text-white max-w-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+        harum inventore omnis voluptatem iste nemo rem? Nam corporis, neque
+        praesentium quisquam accusantium, numquam autem quis doloremque corrupti
+        alias ducimus nulla blanditiis. Quibusdam quos sapiente ex aliquid odit
+        vero vitae dolorum, facilis, officiis iusto commodi itaque, possimus
+        amet quae corrupti doloribus dolor a. Nihil fugiat beatae dolore quis
+        nesciunt odit earum dignissimos officiis velit id eligendi quidem quod,
+        adipisci expedita obcaecati? Animi sunt ipsam ab alias, doloremque
+        reprehenderit harum qui cum repellat itaque. Ipsum quod autem animi
+        labore natus sint consectetur quia aliquam minus expedita, cumque
+        impedit, quisquam eveniet blanditiis officia!
+      </p>
     </div>
   );
 }
