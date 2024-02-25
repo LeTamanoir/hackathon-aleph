d8294acc61b1d4f6b4d79eaea9af6b6545a43a89
LeTamanoir <martin.saldinger@gmail.com>
2024-02-25 12:47:29 +0100

    add better rpc

--- frontend/src/wagmi.ts
+++ frontend/src/wagmi.ts
@@ -6,7 +6,9 @@ export const config = createConfig({
   chains: [sepolia],
   connectors: [injected()],
   transports: {
-    [sepolia.id]: http(),
+    [sepolia.id]: http(
+      "https://sepolia.infura.io/v3/3e52871716db4e138a89863489b59c63"
+    ),
   },
 });
 
